5690a66023d786e4246bd14e790c12868a0692b0
doyo91 <doyo_91@hotmail.com>
2020-09-01 12:25:54 +0200

    Custom Hook and response example

---
+++ src/config/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+export const SEARCH_ID = process.env.REACT_APP_GOOGLE_SEARCH_ID;
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./SearchPage.css";
+import { useStateValue } from "../../context/StateProvider";
+import { useGoogleSearch } from "../../hooks/useGoogleSearch";
+import response from "../../response";
 
 export const SearchPage = () => {
+  const [{ term }, dispatch] = useStateValue();
+  //  LIVE API CALL
+  // const { data } = useGoogleSearch(term);
+
+  // Response example
+  const data = response;
+
+  console.log(data);
   return (
-    <div className="search">
-      <h1>Search Page</h1>
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results"></div>
     </div>
   );
 };
