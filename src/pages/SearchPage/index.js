26ad4d83f4f92cb5f6fc2fd049c67b78a0f634d8
doyo91 <doyo_91@hotmail.com>
2020-09-02 13:14:01 +0200

    Add down bar search page

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -5,6 +5,13 @@ import { useGoogleSearch } from "../../hooks/useGoogleSearch";
 import response from "../../response";
 import { Link } from "react-router-dom";
 import { Search } from "../../components/Search";
+import SearchIcon from "@material-ui/icons/Search";
+import ArtTrackIcon from "@material-ui/icons/ArtTrack";
+import WallpaperIcon from "@material-ui/icons/Wallpaper";
+import SlideshowIcon from "@material-ui/icons/Slideshow";
+import StoreIcon from "@material-ui/icons/Store";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 export const SearchPage = () => {
   const [{ term }, dispatch] = useStateValue();
   //  LIVE API CALL
@@ -27,7 +34,42 @@ export const SearchPage = () => {
         <div className="searchPage__headerBody">
           <Search hideButtons />
 
-          <div className="searchPage__options"></div>
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">Todo</Link>
+              </div>
+              <div className="searchPage__option">
+                <ArtTrackIcon />
+                <Link to="/news">Noticias</Link>
+              </div>
+              <div className="searchPage__option">
+                <WallpaperIcon />
+                <Link to="/images">Imágenes</Link>
+              </div>
+              <div className="searchPage__option">
+                <StoreIcon />
+                <Link to="/shopping">Shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <SlideshowIcon />
+                <Link to="/videos">Vídeos</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">more</Link>
+              </div>
+            </div>
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Configuración</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Herramientas</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="searchPage__results">asfdgdsg</div>
