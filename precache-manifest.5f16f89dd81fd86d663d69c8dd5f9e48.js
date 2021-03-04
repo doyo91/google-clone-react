f44bd15b30dc4e91aefd0068c3d6a4f261a6323e
doyo91 <samuel.gil91dev@gmail.com>
2021-03-04 19:07:16 +0100

    Updates

---
+++ precache-manifest.5f16f89dd81fd86d663d69c8dd5f9e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e94e284eefab95de0634572e188f53",
+    "url": "/google-clone-react/index.html"
+  },
+  {
+    "revision": "d9a15edce8b1c068a152",
+    "url": "/google-clone-react/static/css/main.c5e3c5c8.chunk.css"
+  },
+  {
+    "revision": "3d6cc7eb7eaff8a33c66",
+    "url": "/google-clone-react/static/js/2.1271d2a9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-clone-react/static/js/2.1271d2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a15edce8b1c068a152",
+    "url": "/google-clone-react/static/js/main.96cf08d9.chunk.js"
+  },
+  {
+    "revision": "8da2d99109f1ff5c8dda",
+    "url": "/google-clone-react/static/js/runtime-main.09b4067c.js"
+  }
+]);
